5bde6e4fc1de1b43ec7378d2680fe577cdcdcf9a
Planxty-Dan <dan.newport@detroitlabs.com>
2014-09-30 14:09:04 -0400

    add to methods to put animals in pen, also removes them from main arraylist of all animals

--- src/com/example/Zoo.java
+++ src/com/example/Zoo.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  */
 public class Zoo {
     ArrayList<Pen> allOfThePens= new ArrayList<Pen>();
+    Pen pen = new Pen("");
 
     public void addPen(Pen thisPen) {
         allOfThePens.add(thisPen);
@@ -31,9 +32,11 @@ public class Zoo {
 
     public void putAnimalInPen(Pen thisPen, Animal thisAnimal) {
         thisPen.addAnimal(thisAnimal);
+        pen.allOfTheAnimals.remove(thisAnimal);
     }
 
     public void putBabyAnimalInPen(Pen thisPen, BabyAnimal thisBabyAnimal) {
         thisPen.addBabyAnimal(thisBabyAnimal);
+        pen.allOfTheBabyAnimals.remove(thisBabyAnimal);
     }
 }
